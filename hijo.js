b374d42677c2b4fde5202cb76bd39f703e974410
Felipe segura <yfsegura04@misena.edu.co>
2022-11-10 21:55:39 -0500

    v.1

---
+++ hijo.js
@@ -0,0 +1,11 @@
+Vue.component('Hijo',{
+
+    template://html
+    `
+    <div class="py-5 bg-success">
+    <h4>componente hijo:{{numero}}</h4>
+    </div>
+    ` ,
+    
+  props:['numero']
+  })
